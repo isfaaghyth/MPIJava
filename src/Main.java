2d09d2113d090cd3b6a797b0f777fe356a06c951
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-01-09 04:28:42 +0700

    fix, singlematrix with openMP

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,13 @@ import mpi.*;
 public class Main {
 
     //banyaknya baris dan kolom matriks
-    private static int AMOUNT_MATRIX = 10;
-
-    private static int NUM_ROW_A = AMOUNT_MATRIX;
-    private static int NUM_ROW_B = AMOUNT_MATRIX;
-    private static int NUM_COLUMNS_A = AMOUNT_MATRIX;
-    private static int NUM_COLUMNS_B = AMOUNT_MATRIX;
+    private static int MATRIX_TOTAL = 10;
 
     //penampung
     private static double matA[][]; //penampung matriks A
     private static double matB[][]; //penampung matriks B
+
+    private static double matC[][];
 
     //waktu
     private static double startTime; //waktu mulai
@@ -25,31 +22,65 @@ public class Main {
 
     private static int portion;
 
+    private static int MASTER_TO_SLAVE_TAG = 1;
+    private static int SLAVE_TO_MASTER_TAG = 4;
+
     public static void main(String[] args) {
-        matA = new double[NUM_ROW_A][NUM_COLUMNS_A];
-        matB = new double[NUM_ROW_B][NUM_COLUMNS_B];
+
+        matA = new double[MATRIX_TOTAL][MATRIX_TOTAL];
+        matB = new double[MATRIX_TOTAL][MATRIX_TOTAL];
+        matC = new double[MATRIX_TOTAL][MATRIX_TOTAL];
 
         MPI.Init(args); //inisialisasi MPI
         int rank = MPI.COMM_WORLD.Rank(); //rank
         int size = MPI.COMM_WORLD.Size(); //size
 
-        makeAB(); //inisialisasi value untuk matriks A dan B
-        portion = (NUM_ROW_A / (size - 1)); //menghitung bagian tanpa master
+        portion = (MATRIX_TOTAL / (size - 1)); //menghitung bagian tanpa master
 
-        for (int i=0; i < AMOUNT_MATRIX; i++) {
-            MPI.COMM_WORLD.Bcast(matB[i], AMOUNT_MATRIX*AMOUNT_MATRIX, 0, MPI.INT, 0);
+        if (rank == 0) {
+            makeAB(); //inisialisasi value untuk matriks A dan B
         }
+
+        for (int i=0; i < MATRIX_TOTAL; i++) {
+            MPI.COMM_WORLD.Bcast(matB[i], MATRIX_TOTAL*MATRIX_TOTAL, 0, MPI.INT, 0);
+        }
+
+        for (int i=0; i < size; i++) {
+            for (int j=0; j < MATRIX_TOTAL; j++) {
+                MPI.COMM_WORLD.Send(matA[j], MATRIX_TOTAL*portion, 0, MPI.INT, MASTER_TO_SLAVE_TAG, MASTER_TO_SLAVE_TAG);
+                portion++;
+            }
+        }
+
+        MPI.COMM_WORLD.Recv(matA, MATRIX_TOTAL*portion, 0, MPI.INT, MASTER_TO_SLAVE_TAG, MASTER_TO_SLAVE_TAG);
+
+        startTime = MPI.Wtime();
+        System.out.println("Start time: " + startTime);
+
+        for (int i=0; i < MATRIX_TOTAL; i++) {
+            for (int j=0; j < MATRIX_TOTAL; j++) {
+                matC[i][j] = 0;
+                for (int k=0; k < MATRIX_TOTAL; k++) {
+                    matC[i][j] = matC[i][j] + matA[j][k] * matB[k][i];
+                }
+            }
+        }
+
+        endTime = MPI.Wtime();
+        System.out.println("End time: " + endTime);
+
+
     }
 
     private static void makeAB() {
-        for (int i = 0; i < NUM_ROW_A; i++) {
-            for (int j = 0; j < NUM_COLUMNS_A; j++) {
+        for (int i = 0; i < MATRIX_TOTAL; i++) {
+            for (int j = 0; j < MATRIX_TOTAL; j++) {
                 matA[i][j] = i + j;
             }
         }
-        for (int i = 0; i < NUM_ROW_B; i++) {
-            for (int j = 0; j < NUM_COLUMNS_B; j++) {
-                matB[i][j] = i*j;
+        for (int i = 0; i < MATRIX_TOTAL; i++) {
+            for (int j = 0; j < MATRIX_TOTAL; j++) {
+                matB[i][j] = i * j;
             }
         }
     }
---
+++ src/SingleMatrix.java
@@ -0,0 +1,121 @@
+import mpi.*;
+
+import java.util.Random;
+
+/**
+ * Single Matrix with openMP
+ * @porting isfaaghyth
+ * @reference: https://github.com/JING-TIME/C-Programming/blob/master/141029/MatrixMul.c
+ * @reference: https://gist.github.com/morris821028/1ee07f52d494217ae26933d352c7f07f
+ */
+public class SingleMatrix {
+
+    private static final int N = 8;
+
+    private static int matA[] = new int[N * N];
+    private static int matB[] = new int[N * N];
+
+    //penampung
+    private static int matC[] = new int[N * N];
+
+    private static int porsi;
+
+    public static void main(String[] args) {
+        MPI.Init(args); //inisialisasi openMP
+
+        int rank = MPI.COMM_WORLD.Rank(); //get rank
+        int size = MPI.COMM_WORLD.Size(); //get size
+
+        //mengisi array matriks A dan B
+        //@Using: random range dari 0 s/d 4
+        if (rank == 0) {
+            for (int i=0; i < N; i++) {
+                for (int j=0; j < N; j++) {
+                    matA[i * N + j] = new Random().nextInt(4);
+                    matB[i * N + j] = new Random().nextInt(4);
+                }
+            }
+            printMatrix("A", matA);
+            printMatrix("B", matB);
+        }
+
+        porsi = N / size;
+
+        int[] matAi = new int[porsi * N];
+        int[] matBi = new int[N * N];
+        int[] matCi = new int[porsi * N];
+
+        //mengirim matriks A dari mainProcess(main) ke otherProcess (slave)
+        if (rank == 0) {
+            for (int i=0; i < porsi; i++) {
+                for (int j=0; j < N; j++) {
+                    matAi[i * N + j] = matA[i * N + j];
+                }
+            }
+            printMatrix("A baru", matA);
+            for (int i=1; i < size; i++) {
+                MPI.COMM_WORLD.Send(matA, i * porsi * N, porsi * N, MPI.INT, i, i);
+            }
+        } else {
+            MPI.COMM_WORLD.Recv(matAi, 0, porsi * N, MPI.INT, 0, rank);
+        }
+
+        //mengirim matriks B dari mainProcess(main) ke otherProcess (slave)
+        if (rank == 0) {
+            for (int i=0; i < N; i++) {
+                for (int j=0; j < N; j++) {
+                    matBi[i * N + j] = matB[i * N + j];
+                }
+            }
+            for (int i=1; i < size; i++) {
+                MPI.COMM_WORLD.Send(matB, 0, N * N, MPI.INT, i, i);
+            }
+        } else {
+            MPI.COMM_WORLD.Recv(matBi, 0, N * N, MPI.INT, 0, rank);
+        }
+
+        //kalkulasi !
+        for (int i=0; i < porsi; i++) {
+            for (int j=0; j < N; j++) {
+                matCi[i * N + j] = 0;
+                for (int k=0; k < N; k++) {
+                    matCi[i * N + j] += matAi[i * N + k] * matBi[k * N + j];
+                }
+            }
+        }
+
+        //kirim hasil dari otherProcess(slave) ke mainProcess(master)
+        if (rank != 0) {
+            MPI.COMM_WORLD.Send(matCi, 0, porsi * N, MPI.INT, 0, rank);
+        } else {
+            for (int i=0; i < porsi; i++) {
+                for (int j=0; j < N; j++) {
+                    matC[i * N + j] = matCi[i * N + j];
+                }
+            }
+            for (int i=1; i < size; i++) {
+                MPI.COMM_WORLD.Recv(matC, i * porsi * N, porsi * N, MPI.INT, i, i);
+            }
+        }
+
+        //tampilkan hasil
+        if (rank == 0) {
+            System.out.println("Result");
+            printMatrix("C", matC);
+        }
+
+        //done! :)
+        MPI.Finalize();
+    }
+
+    private static void printMatrix(String pos, int arr[]) {
+        System.out.println("MATRIKS " + pos);
+        for (int i=0; i < N; i++) {
+            for (int j=0; j < N; j++) {
+                System.out.print(arr[i*N+j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
