307eb74c7586bd9cdf76c0a69dbdbef4102589b1
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-01-09 00:21:14 +0700

    remake from practice C++

---
+++ src/Convert.java
@@ -0,0 +1,30 @@
+import mpi.*;
+
+/**
+ * just backup :)
+ */
+public class Convert {
+
+    public void convertRes(String[] args) {
+        MPI.Init(args);
+        int myRank = MPI.COMM_WORLD.Rank();
+        if(myRank == 0) {
+            char[]message = "Hello there".toCharArray();
+            MPI.COMM_WORLD.Send(message, 0, message.length, MPI.CHAR, 1, 99);
+        } else {
+            char[] message = new char [20];
+            MPI.COMM_WORLD.Recv(message, 0, 20, MPI.CHAR, 0, 99);
+            System.out.println("received:" + new String(message) + ":");
+        }
+        MPI.Finalize();
+    }
+
+    public void helloWorldRes(String[] args) {
+        MPI.Init(args);
+        int myRank = MPI.COMM_WORLD.Rank();
+        int mySize = MPI.COMM_WORLD.Size();
+        System.out.println("Hello " + MPI.Get_processor_name() + " rank " + myRank + " size " + mySize);
+        MPI.Finalize();
+    }
+
+}
